427ad4513d6281d6deee9abce83f7ccc7654a271
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-03-17 20:23:49 +0500

    api link changes

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const getLatLon = createAsyncThunk(
   'city/LatLon',
   async (cityName) => {
-    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=f617f62839e8a87840642b3875ecf15e`);
+    const response = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=f617f62839e8a87840642b3875ecf15e`);
     if (response.data[0] === undefined) {
       return { error: 'please correct your city name !' };
     }
